41e8c12f9bbfea242a2e1d7587860df46bcb525f
pqx <l.r.slegt@gmail.com>
2022-08-10 21:35:21 +0200

    ✨ Add sidebar Fixes #7

---
+++ src/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
+import PeopleIcon from '@mui/icons-material/People';
+import ChatIcon from '@mui/icons-material/Chat';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <SidebarRow src='https://avatars.githubusercontent.com/u/24712956?v=4' title='Sonny Sangha'/>
+      <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />      
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Marketplace" />
+
+    </div>
+  );
+}
+
+export default Sidebar;
