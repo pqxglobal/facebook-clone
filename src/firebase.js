f8af10f6a5d0f90b2b06dd8b0a0fb1033496f5ea
pqx <l.r.slegt@gmail.com>
2022-08-10 20:20:01 +0200

    Add Header Fixes #1

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAH-xmjEMifndSpMl9LNKYhRUuT7bz6Uw8",
+  authDomain: "facebook-clone-d455c.firebaseapp.com",
+  projectId: "facebook-clone-d455c",
+  storageBucket: "facebook-clone-d455c.appspot.com",
+  messagingSenderId: "306171559507",
+  appId: "1:306171559507:web:84ff7c449030862bcb10a6",
+  measurementId: "G-5BGY6ZXWJG"
+};
