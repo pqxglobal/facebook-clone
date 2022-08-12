89eef52987fb434e1fdd9a3f3af338866d20ee33
pqx <l.r.slegt@gmail.com>
2022-08-12 17:46:20 +0200

    final

--- src/Feed.js
+++ src/Feed.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import MessageSender from './MessageSender';
 import Post from './Post';
 import StoryReel from './StoryReel';
+import db from './firebase';
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+  }, []);
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
 
-      <Post
-        profilePic="https://avatars.githubusercontent.com/u/24712956?v=4"
-        message="WOW this dummy works"
-        timestamp="This is a dummy timestamp"
-        username="sssssangha"
-        image="https://code.org/shared/images/social-media/codeorg2019_social.png"
-      />
-      <Post
-        profilePic="https://avatars.githubusercontent.com/u/24712956?v=4"
-        message="WOW this dummy works"
-        timestamp="This is a dummy timestamp"
-        username="sssssangha"
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
   );
 }
